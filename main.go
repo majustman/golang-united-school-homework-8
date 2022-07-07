e011e67585e91970982dcc60d7d7750c11ae5e66
samatt90 <samatt90@gmail.com>
2022-07-07 17:54:24 +0600

    fix: parseArg

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"os"
 )
@@ -19,23 +21,35 @@ func main() {
 }
 
 func parseArgs() Arguments {
-	var res Arguments = make(map[string]string)
-	args := os.Args[1:]
-	for i, arg := range args {
-		switch arg {
-		case "-operation":
-			if i != len(args)-1 && args[i+1][0] != '-' {
-				res[arg[1:]] = args[i+1]
-			}
-		case "-item":
-			if i != len(args)-1 && args[i+1][0] != '-' {
-				res[arg[1:]] = args[i+1]
-			}
-		case "-fileName":
-			if i != len(args)-1 && args[i+1][0] != '-' {
-				res[arg[1:]] = args[i+1]
-			}
-		}
-	}
+	var (
+		res                          Arguments = make(map[string]string)
+		operFlag, itemFlag, fileFlag string
+	)
+	//args := os.Args[1:]
+	// for i, arg := range args {
+	// 	switch arg {
+	// 	case "-operation":
+	// 		if i != len(args)-1 && args[i+1][0] != '-' {
+	// 			res[arg[1:]] = args[i+1]
+	// 		}
+	// 	case "-item":
+	// 		if i != len(args)-1 && args[i+1][0] != '-' {
+	// 			res[arg[1:]] = args[i+1]
+	// 		}
+	// 	case "-fileName":
+	// 		if i != len(args)-1 && args[i+1][0] != '-' {
+	// 			res[arg[1:]] = args[i+1]
+	// 		}
+	// 	}
+	// }
+	flag.StringVar(&operFlag, "operation", "", "value for the operation argument")
+	flag.StringVar(&itemFlag, "item", "", "value for the item argument")
+	flag.StringVar(&fileFlag, "fileName", "", "value for the fileName argument")
+	flag.Parse()
+	res["operation"] = operFlag
+	res["item"] = itemFlag
+	res["fileName"] = fileFlag
+	fmt.Println(res)
+
 	return res
 }
