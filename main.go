14872930b6e1899e2d9a225e86755bcba58b6f0a
samatt90 <samatt90@gmail.com>
2022-07-07 16:24:29 +0600

    init: parseArg func

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"os"
 )
 
+type Arguments map[string]string
+
 func Perform(args Arguments, writer io.Writer) error {
+	return nil
 }
 
 func main() {
@@ -13,4 +16,26 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func parseArgs() Arguments {
+	var res Arguments = make(map[string]string)
+	args := os.Args[1:]
+	for i, arg := range args {
+		switch arg {
+		case "-operation":
+			if i != len(args)-1 && args[i+1][0] != '-' {
+				res[arg[1:]] = args[i+1]
+			}
+		case "-item":
+			if i != len(args)-1 && args[i+1][0] != '-' {
+				res[arg[1:]] = args[i+1]
+			}
+		case "-fileName":
+			if i != len(args)-1 && args[i+1][0] != '-' {
+				res[arg[1:]] = args[i+1]
+			}
+		}
+	}
+	return res
 }
